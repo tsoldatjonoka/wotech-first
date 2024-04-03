20c14feb7aa152c577990157b542bfabb2c7b67b
tsoldatjonoka <165931857+tsoldatjonoka@users.noreply.github.com>
2024-04-03 18:19:28 +0200

    Create main.java

---
+++ main.java
@@ -0,0 +1,31 @@
+// import static org.junit.jupiter.api.Assertions.assertEquals;
+
+// import org.junit.jupiter.api.Test;
+
+public class Main {
+  public static void main(String[] args) {    
+    System.out.println("Hello world!");
+    // winter, spring, summer, autumn
+    // warm jacket, t-shirt, swimming suite, rain coat
+    String season = "autumn";
+
+    if (season == "winter") {
+      System.out.println("Wear a warm jacket!");
+    }
+    if (season == "spring") {
+      System.out.println("Wear a T-shirt!");
+    }
+    if (season == "summer") {
+      System.out.println("Wear a swimming suite!");
+    }
+    if (season == "autumn") {
+      System.out.println("Wear a rain coat!");
+    }
+
+  }
+
+  // @Test
+  // void addition() {
+  //     assertEquals(2, 1 + 1);
+  // }
+}
